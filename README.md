ae3eb0c4bfd05e2e4abedf5cd27af0cda869af49
Kenneth Hernane <hernanekennethhumphrey@gmail.com>
2023-05-02 10:58:28 +0800

    Updated Files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CRUD-expressjs
+To open, in terminal, run 'npm run dev'
+THe fields are case sensitive, use the exact case for searching
--- routes/api/member.js
+++ routes/api/member.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const uuid = require('uuid');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const members = require('../../Members');
 
+router.use(bodyParser.urlencoded({ extended: true }));
 router.get('/', (req, res) => res.json(members));
 
 router.get('/:id', (req, res) => {
@@ -31,19 +33,39 @@ router.post('/', (req, res) => {
    res.redirect('/');
 });
 
-router.put('/:id', (req, res) => {
-    const found = members.some(member => member.id === parseInt(req.params.id));
+router.post('/update', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.body.id));
+    console.log(req.body.id);
 
     if(found) {
         const updMember = req.body;
         members.forEach(member => {
-            if(member.id === parseInt(req.params.id)) {
+            if(member.id === parseInt(req.body.id)) {
                 member.name = updMember.name ? updMember.name : member.name;
                 member.email = updMember.email ? updMember.email : member.email;
-
-                res.json({ msg: "Member Updated", member });
             }
         });
+        res.redirect('/');
+    }
+    else {
+        res.status(400).json({ msg: `No member with the id of ${req.body.id}`});
+    }
+});
+
+router.post('/remove', (req, res) => {
+    const found = members.some(member => member.name === req.body.name);
+    if(found) {
+        var index = -1;
+        var val = req.body.name;
+        console.log(val);
+        var filter = members.find(function (item, i) {
+            if(item.name === val) {
+                index = i;
+                return i;
+            }
+        });
+        members.splice(index, 1);
+        res.redirect('/');
     }
     else {
         res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
